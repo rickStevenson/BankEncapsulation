bb4ad1426fe445592339fd70803896fcf979d4de
RickStevenson <dadsathome1@gmail.com>
2024-09-11 11:17:37 -0600

    Finished Exercise 1

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankEncapsulation
+{
+    internal class BankAccount
+    {
+        private double _balance = 0;
+
+        public double Deposit( double deposit)
+        {
+            return _balance += deposit;
+        }
+        public double GetBalance()
+        {
+            return _balance;
+        }
+    }
+}
--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -1,10 +1,45 @@
-﻿namespace BankEncapsulation
+﻿using System.Transactions;
+
+namespace BankEncapsulation
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            CustomerTransactions();
+        }
+        public static void CustomerTransactions()
+        {
+            var bankAccount1 = new BankAccount();
+
+            string response;
+
+            do
+            {
+                Console.WriteLine("What would you like to do?");
+                Console.WriteLine("Please type 'See Balance' or 'Make Deposit' or 'Exit'");
+                response = Console.ReadLine().ToLower();
+                Console.WriteLine(response);
+                if (response == "see balance")
+                {
+                    double balance = bankAccount1.GetBalance();
+                    Console.WriteLine($"Your balance is {balance}");
+                }
+                else if (response == "make deposit")
+                {
+                    Console.WriteLine("Please enter deposit amount.");
+                    double _depositAmount = double.Parse(Console.ReadLine());
+                    bankAccount1.Deposit(_depositAmount);
+                }
+                else if (response == "exit")
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Did not recognize that response.");
+                };
+            } while (response != "see balance" || response != "make deposit");
         }
     }
 }
